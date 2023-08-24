e078ec93f0b00dbce9530d6d09849b4fd0e703ef
hanyarifky <rifkydoang2014@gmail.com>
2023-08-24 08:58:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        handjet: ['Handjet'],
+      },
+      colors: {
+        krem: '#ffe4e6',
+      },
+    },
+  },
+  plugins: [],
+};
